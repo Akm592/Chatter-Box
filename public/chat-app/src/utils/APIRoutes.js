1779bdba4d485b42a3325bc069a7faf66e115269
ASHISH KUMAR MISHRA <95633624+Akm592@users.noreply.github.com>
2024-02-08 00:54:43 +0530

    Update APIRoutes.js

--- public/chat-app/src/utils/APIRoutes.js
+++ public/chat-app/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatter-box-vh95.onrender.com";
+export const host = "https://chatter-box-vh95.onrender.com:10000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
